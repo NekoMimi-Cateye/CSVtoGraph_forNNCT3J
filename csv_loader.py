8feb25096c006a965c85b57c3e00764f67366f0f
NekoMimi-Cateye <umakko.lumiere@icloud.com>
2020-10-29 19:54:50 +0900

    v2.0 release - note: 4Fオシロスコープ用にハードコード化

--- csv_loader.py
+++ csv_loader.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 print('【はじめに】Pythonユーザー: csvファイルをこのexeファイルと同じディレクトリに入れてください')
 print('【はじめに】Colaboratoryからの実行: csvファイルをMy Drive直下に入れてください')
-print('【はじめに】csvファイルの表以外の部分(ヘッダーとフッダー)を削除してください\n')
+print('【はじめに】csvファイルの表以外の部分(ヘッダーとフッダー)を削除してください\n\n')
+
 print('ファイル名を入力してください.  例) ./hoge.csv  注)./ は必要')
 print('ファイル名: ', end='')
 path = input()
@@ -21,69 +22,55 @@ x_tag = 0
 for i in range(len(df.columns)):
     data_tag.append(df_h[i][0])
 
-while(1):
-    print('横軸にしたいデータ列のタグを選択してください (タグ: ', end='')
-    for i in data_tag:
-        print('{:s}, '.format(i), end='')
-    print(')')
-    check = input()
-    if (check in data_tag):
-        nx = df[check]
-        x_tag = check
-        break
-    else:
-        print('[ERROR] \'{:s}\' という名前のタグは見つかりません. もう一度選択してください'.format(check))
-
 nx = df['TIME']
 ny = []
 
-print('\n縦軸にするデータ列を選んでいきます.')
+print('\nグラフにするデータ列を選んでいきます.')
 for i in data_tag:
-    if (i == x_tag):
+    if (i == "TIME"):
         continue
     else:
-        print('{:s}を横軸, {:s}を縦軸としてグラフを作成しますか? (はい: yes , いいえ: その他のコマンド(何かを入力))'.format(x_tag, i))
+        print('TIMEを横軸, {:s}を縦軸としてグラフを作成しますか? (はい: yes , いいえ: その他のコマンド(何かを入力))'.format(i))
         check = input()
         if (check == 'yes'):
             ny.append(df[i])
 
-print('\nグラフの題名を入力してください')
+print('\nタイトルを入力 >>> ', end='')
 title = input()
 
 print('\nグラフを作成しています')
-print('作成完了 保存先: ./result.png')
 
+cs = ["m", "u", "n"]
+cb = ["k", "M", "G"]
 x = []
 y = [[] for i in range(len(ny))]
 for i in nx:
     x.append(i)
 
+xmin = x[0]
+for i in range(len(x)):
+    x[i] -= xmin
+
+xmax = x[len(x)-1]
+xc = 0
+while xmax < 1:
+    xmax *= 10
+    xc += 1
+c = xc // 3 + 1
+
+for i in range(len(x)):
+    x[i] *= 10 ** (3 * c)
+xc += 1
+
+
+
 for i in range(len(ny)):
     for j in ny[i]:
         y[i].append(j)
 
-plt.figure(figsize = (12, 12))
-plt.rcParams["font.family"] = "Times New Roman"
-plt.rcParams["xtick.direction"] = "in"
-plt.rcParams["ytick.direction"] = "in"
-plt.rcParams["xtick.minor.visible"] = True
-plt.rcParams["ytick.minor.visible"] = True
-plt.rcParams["xtick.major.width"] = 1.5
-plt.rcParams["ytick.major.width"] = 1.5
-plt.rcParams["xtick.minor.width"] = 1.0
-plt.rcParams["ytick.minor.width"] = 1.0
-plt.rcParams["xtick.major.size"] = 10
-plt.rcParams["ytick.major.size"] = 10
-plt.rcParams["xtick.minor.size"] = 5
-plt.rcParams["ytick.minor.size"] = 5
-plt.rcParams["font.size"] = 14
-plt.rcParams["axes.linewidth"] = 1.5
-
+plt.figure(figsize = (12, 2.5*len(y)))
 for i in range(len(y)):
     plt.subplot(len(y), 1, i+1)
-    if (i == 0):
-        plt.title(title)
-    plt.tick_params(labelbottom=False)
     plt.rcParams["font.family"] = "Times New Roman"
     plt.rcParams["xtick.direction"] = "in"
     plt.rcParams["ytick.direction"] = "in"
@@ -99,10 +86,33 @@ for i in range(len(y)):
     plt.rcParams["ytick.minor.size"] = 5
     plt.rcParams["font.size"] = 14
     plt.rcParams["axes.linewidth"] = 1.5
+    if (i == 0):
+        plt.title(title)
+    plt.tick_params(labelbottom=False)
     plt.ylim((-2.5, 7.5))
+    index = 0
+    while (y[i][index] < 5):
+        index += 1
+    fs = index
+    while (y[i][index] > 0):
+        index += 1
+    while (y[i][index] < 5):
+        index += 1
+    fe = index
+
+    while (y[i][fs] < 2.5):
+        fs += 1
+    while (y[i][fs] < 2.5):
+        fe += 1
+    p1 = plt.vlines([x[fs]], -2.5, 7.5, "black", linestyles='dashed')
+    p2 = plt.vlines([x[fe]], -2.5, 7.5, "black", linestyles='dashed')
+    plt.text(x[fe], 6.8, "T={:3.2f}[{:s}s], f={:3.2f}[{:s}Hz]".format(x[fe]-x[fs], cs[c], 1000 / (x[fe]-x[fs]) , cb[c-2]), size=8)
+    plt.ylabel("Voltage [V]")
     plt.plot(x, y[i], color = "black")
 plt.tick_params(labelbottom=True)
+plt.xlabel("time [{:s}s]".format(cs[c]))
 
+print('作成完了 保存先: ./result.png')
 plt.savefig('result.png')
 print("Successed!")
 plt.show()
